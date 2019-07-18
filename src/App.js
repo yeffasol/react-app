79b6c65f396e46a3e99964acead8e0c31c70171c
Marat <yeffasol@mail.ru>
2019-07-18 18:39:23 +0300

    Update example user-card to redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {createStore} from 'redux';
+import reducer from './reducers';
+import user from "./images/user.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+    name: "Alex Bakery",
+    description: "Software Engineer, Speaker, and Occasional Model",
+    likes: "Cats, Wine, and Black dresses",
+    location: "Kazan"
+};
+const store = createStore(reducer, initialState);
+
+class App extends Component {
+    state = store.getState();
+
+    render() {
+        const {name, description, likes, location} = this.state;
+        return (
+            <div className="App">
+                <section className="User__img">
+                    <img src={user} alt="user"/>
+                </section>
+
+                <section className="User__info">
+                    <p>
+                        {" "}
+                        <span className="faint">I am</span> a {description}
+                    </p>
+                    <p>
+                        {" "}
+                        <span className="faint">I like</span> {likes}
+                    </p>
+
+                    <p className="User__info__details User__info__divider faint">
+                        <span>Name: </span>
+                        <span>{name}</span>
+                    </p>
+                    <p className="User__info__details faint">
+                        <span>Location: </span>
+                        <span>{location}</span>
+                    </p>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
