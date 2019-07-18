8f4a4a11eb9691066ff49f93cf9d83b66e3e5865
Marat <yeffasol@mail.ru>
2019-07-18 17:49:56 +0300

    Updates

---
+++ precache-manifest.a09178279e25dd82313302463c63840d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c2991f0d8dcc0e7c8ad233f9a01c48",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "41a03378c537bc6319c9",
+    "url": "/react-app/static/css/main.62f412b1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "41a03378c537bc6319c9",
+    "url": "/react-app/static/js/main.f45fd67a.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "fd2aafe858928e0125a14f8b357c683f",
+    "url": "/react-app/static/media/hello.fd2aafe8.png"
+  }
+]);
