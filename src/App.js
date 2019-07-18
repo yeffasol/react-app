e2eca64f85d99e76854ebbb283e0bc9da54160b0
Marat <yeffasol@mail.ru>
2019-07-18 19:11:09 +0300

    Add actions in hello

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import HelloWorld from "./HelloWorld";
-import reducer from "./reducers";
-import { createStore } from "redux"; //an import from the redux library
-
-const initialState = { tech: "Marat" };
-
-const store = createStore(reducer,initialState);
+import ButtonGroup from "./ButtonGroup";
+import {store} from "./store";
 
 class App extends Component {
 
-  render() {
-    return <HelloWorld tech={store.getState().tech} />;
-  }
+    render() {
+        //With React 16, you don’t have to wrap adjacent JSX elements in a div.
+        // You can use an array if you want — but pass in a key prop to each element in the array.
+        return [
+            <HelloWorld key={1} tech={store.getState().tech}/>,
+            <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]}/>
+        ]
+    }
 }
 
 export default App;
